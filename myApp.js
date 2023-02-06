98f4179af48b98c6f9c639ad04e15e33223a183d
Mohit Patel <mohit.patel1966@gmail.com>
2023-02-06 21:05:01 +0530

    added input from client

--- myApp.js
+++ myApp.js
@@ -3,16 +3,17 @@ let app = express();
 require('dotenv').config()
 
 console.log("Hello World");
-// const handler = (req,res)=>{
-//     // res.send("Hello Express")
-//     res.sendFile(__dirname+'/views/index.html')
-// } 
 
 app.use((req,res,next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     console.log("hello")
     next()
 
+})
+
+app.get('/:word/echo',(req,res) => {
+    const { params } = req;
+    res.json({echo: params.word})
 })
 
 app.get('/now',(req,res,next) => {
@@ -23,7 +24,11 @@ app.get('/now',(req,res,next) => {
 })
 
 app.use('/public',express.static(__dirname+'/public'))
-// app.get('/',handler)
+const handler = (req,res)=>{
+    // res.send("Hello Express")
+    res.sendFile(__dirname+'/views/index.html')
+} 
+app.get('/',handler)
 app.get('/json',(req,res)=>{
     if(process.env.MESSAGE_STYLE==='uppercase' ? message="HELLO JSON" : "Hello json"){
         res.json( {message} )
